acac1bef57667929f2d3dd4dcec0fa36acc2e03d
Titiushko <titiushko@gmail.com>
2018-06-14 11:05:57 -0600

    :bomb: Creando extensión personalizada para excepciones.

---
+++ Titiushko/Titiushko.Utilities/Extensions/Exceptions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
+
+namespace Titiushko.Utilities.Extensions
+{
+    public static class Exceptions
+    {
+        /// <summary>
+        /// Get exception messages
+        /// </summary>
+        /// <param name="pException">Exception</param>
+        /// <returns>Messages</returns>
+        public static string GetExceptionMessage(this Exception pException)
+        {
+            return string.Join("<br>", GetExceptionMessages(pException, new HashSet<string>()));
+        }
+
+        /// <summary>
+        /// Get exception messages list
+        /// </summary>
+        /// <param name="pException">Exception</param>
+        /// <param name="pMessages">Messages list</param>
+        /// <returns>Messages list</returns>
+        private static ICollection<string> GetExceptionMessages(Exception pException, ICollection<string> pMessages)
+        {
+            pMessages.Add(pException.Message);
+            if (pException.InnerException != null) pMessages = GetExceptionMessages(pException.InnerException, pMessages);
+            return pMessages;
+        }
+
+        /// <summary>
+        /// Get database entity validation exception messages
+        /// </summary>
+        /// <param name="pEntityException">DbEntityValidationException</param>
+        /// <returns>Messages</returns>
+        public static string GetEntityExceptionMessage(this DbEntityValidationException pEntityException)
+        {
+            ICollection<string> vMessages = new HashSet<string>();
+            string vMessageAux = string.Empty;
+            foreach (DbEntityValidationResult validationErrors in pEntityException.EntityValidationErrors)
+            {
+                foreach (DbValidationError validationError in validationErrors.ValidationErrors)
+                {
+                    if (!validationError.ErrorMessage.Equals(vMessageAux))
+                    {
+                        vMessageAux = validationError.ErrorMessage;
+                        vMessages.Add(vMessageAux);
+                    }
+                }
+            }
+            return string.Join("<br>", vMessages);
+        }
+
+        /// <summary>
+        /// Get SQL exception number
+        /// Some more known:
+        /// 2627: Unique constraint error
+        /// 547: Constraint check violation
+        /// 2601: Duplicated key row error
+        /// </summary>
+        /// <param name="pException">Exception</param>
+        /// <returns>Int or null</returns>
+        public static int? SqlExceptionNumber(this Exception pException)
+        {
+            DbUpdateConcurrencyException vConcurrencyException = pException as DbUpdateConcurrencyException;
+            DbUpdateException vDbException = pException as DbUpdateException;
+            if (vDbException != null && vDbException.InnerException != null && vDbException.InnerException.InnerException != null)
+            {
+                SqlException vSqlException = vDbException.InnerException.InnerException as SqlException;
+                if (vSqlException != null) return vSqlException.Number;
+            }
+            return null;
+        }
+    }
+}
