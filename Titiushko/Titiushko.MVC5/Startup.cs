3e4e832129cb75d9efc351f72ae662511056f03f
Titiushko <titiushko@gmail.com>
2018-06-14 07:13:37 -0600

    :octopus: Proyecto de ejemplo en MVC5.

---
+++ Titiushko/Titiushko.MVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Titiushko.MVC5.Startup))]
+namespace Titiushko.MVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
