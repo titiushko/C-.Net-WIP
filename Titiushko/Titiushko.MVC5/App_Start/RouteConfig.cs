5697fbae2f252e1d7d961529872437942cfbbb6e
Titiushko <titiushko@gmail.com>
2018-06-18 08:56:43 -0600

    :movie_camera: Configuración para definir rutas personalizadas.

--- Titiushko/Titiushko.MVC5/App_Start/RouteConfig.cs
+++ Titiushko/Titiushko.MVC5/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Titiushko.MVC5
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
