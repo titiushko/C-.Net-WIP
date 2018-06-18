30aa54d00fcb1ed692b32afc503f528b7695e7eb
Titiushko <titiushko@gmail.com>
2018-06-18 12:00:27 -0600

    :dog2: Actions para devolver excepciones a las vistas.

--- Titiushko/Titiushko.MVC5/Controllers/BaseController.cs
+++ Titiushko/Titiushko.MVC5/Controllers/BaseController.cs
@@ -1,9 +1,7 @@
 ﻿using log4net;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Titiushko.Utilities.Extensions;
 
 namespace Titiushko.MVC5.Controllers
 {
@@ -15,5 +13,69 @@ namespace Titiushko.MVC5.Controllers
         {
             logger = LogManager.GetLogger(typeof(BaseController));
         }
+
+        #region Exceptions
+        /// <summary>
+        /// Get TempDataDictionary with exception messages
+        /// </summary>
+        /// <param name="pE">Exception</param>
+        /// <returns>TempDataDictionary</returns>
+        public TempDataDictionary TempDataExceptionMessage(Exception pE)
+        {
+            TempDataDictionary vError = new TempDataDictionary();
+            vError["error"] = Titiushko.Utilities.Constants.Basic.TRUE;
+            vError["error_message"] = Titiushko.Utilities.Constants.Errors.BaseError.DEFAULT(pE.GetExceptionMessage());
+            return vError;
+        }
+
+        /// <summary>
+        /// Get TempDataDictionary with database entity exception messages
+        /// </summary>
+        /// <param name="pE">DbEntityValidationException</param>
+        /// <returns>TempDataDictionary</returns>
+        public TempDataDictionary TempDataEntityExceptionMessage(System.Data.Entity.Validation.DbEntityValidationException pE)
+        {
+            TempDataDictionary vError = new TempDataDictionary();
+            vError["error"] = Titiushko.Utilities.Constants.Basic.TRUE;
+            vError["error_message"] = Titiushko.Utilities.Constants.Errors.BaseError.DEFAULT(pE.GetEntityExceptionMessage());
+            return vError;
+        }
+
+        /// <summary>
+        /// Process the request to register in the log the exception occurred in JS from the frontend
+        /// </summary>
+        /// <param name="pException"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateInput(false)]
+        [Route("log-exception")]
+        public JsonResult LogExceptionFromJavaScript(string pException)
+        {
+            try
+            {
+                LogManager.GetLogger("BaseController.LogExceptionFromJavaScript").Error(new Exception(pException));
+                return Json(new Titiushko.Utilities.Responses.JsonResponse());
+            }
+            catch (Exception vE)
+            {
+                return Json(Titiushko.Utilities.Constants.Errors.JsonError.EXCEPCION(vE));
+            }
+        }
+
+        public ActionResult Error404(bool pIsPartialView = false)
+        {
+            string vViewTemplate = "~/Views/Errors/404.cshtml";
+            if (pIsPartialView)
+            {
+                ViewBag.Layout = null;
+                return PartialView(vViewTemplate);
+            }
+            else
+            {
+                ViewBag.Layout = "~/Views/Shared/_Layout.cshtml";
+                return View(vViewTemplate);
+            }
+        }
+        #endregion
     }
 }
