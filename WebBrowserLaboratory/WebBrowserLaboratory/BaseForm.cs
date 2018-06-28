ccde45708f417b0c41bd01ad67c6cf2f63062c6c
titiushko <titiushko@gmail.com>
2018-06-28 15:54:29 -0600

    :black_joker: Proyecto para probar System.Windows.Forms.WebBrowser.

---
+++ WebBrowserLaboratory/WebBrowserLaboratory/BaseForm.cs
@@ -0,0 +1,93 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace WebBrowserLaboratory
+{
+    public class BaseForm : Form
+    {
+        bool _PageLoaded = false;
+        bool _OnLoadEventSet = false;
+
+        protected void RunActionAndWaitUntilPageLoaded(WebBrowser pBrowser, Action pActionToRun)
+        {
+            try
+            {
+                string vCurrentHtml = pBrowser.DocumentText;
+                _PageLoaded = false;
+                _OnLoadEventSet = false;
+                pBrowser.DocumentCompleted += BrowserDocumentCompleted;
+                //AddOnLoadEventToIFrames(pBrowser);
+                if (pActionToRun != null)
+                {
+                    pActionToRun.Invoke();
+                    while (!_PageLoaded)
+                    {
+                        Application.DoEvents();
+                        _PageLoaded = (pBrowser.ReadyState.Equals(WebBrowserReadyState.Complete) && !vCurrentHtml.Equals(pBrowser.DocumentText));
+                    }
+                    pBrowser.DocumentCompleted -= BrowserDocumentCompleted;
+                }
+            }
+            catch (Exception vE)
+            {
+                throw vE;
+            }
+        }
+
+        public void AddOnLoadEventToIFrames(WebBrowser pBrowser)
+        {
+            try
+            {
+                // Los frames no son visibles en el texto del documento, importante para ver si algo cambia allí también
+                if (pBrowser.Document.Window != null && pBrowser.Document.Window.Frames != null && pBrowser.Document.Window.Frames.Count > 0)
+                {
+                    for (int vCounter = 0; vCounter < pBrowser.Document.Window.Frames.Count; vCounter++)
+                    {
+                        try
+                        {
+                            pBrowser.Document.Window.Frames[vCounter].WindowFrameElement.AttachEventHandler("onload", delegate
+                            {
+                                // Aplazar esto para asegurarse de que todos los controladores de eventos de carga posibles sean despedidos
+                                System.Threading.SynchronizationContext.Current.Post(delegate
+                                {
+                                    _PageLoaded = true;
+                                }, null);
+                            });
+                        }
+                        catch (Exception vE)
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+            catch (Exception vE)
+            {
+                throw vE;
+            }
+        }
+
+        private void BrowserDocumentCompleted(object pSender, WebBrowserDocumentCompletedEventArgs pEvent)
+        {
+            try
+            {
+                var vBrowser = pSender as WebBrowser;
+                if (_OnLoadEventSet) return;
+                _OnLoadEventSet = true;
+                // DocumentCompleted se activa antes de window.onload y body.onload
+                vBrowser.Document.Window.AttachEventHandler("onload", delegate
+                {
+                    // Aplazar esto para asegurarse de que todos los controladores de eventos de carga posibles sean despedidos
+                    System.Threading.SynchronizationContext.Current.Post(delegate
+                    {
+                        _PageLoaded = true;
+                    }, null);
+                });
+            }
+            catch (Exception vE)
+            {
+                throw vE;
+            }
+        }
+    }
+}
---
+++ WebBrowserLaboratory/WebBrowserLaboratory/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System;
+using WebBrowserLaboratory.Helpers;
+using System.Configuration;
+
+namespace WebBrowserLaboratory
+{
+    public partial class Form1 : BaseForm
+    {
+        private const string URL = "http://54.173.64.156/";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.urlTextBox.Text = URL;
+            this.webBrowser.Navigate(this.urlTextBox.Text);
+        }
+
+        private void GoButton_Click(object pSender, EventArgs pEvent)
+        {
+            try
+            {
+                this.Login();
+            }
+            catch (System.Exception vE)
+            {
+
+            }
+        }
+
+        private void NavigateButton_Click(object pSender, EventArgs pEvent)
+        {
+            this.webBrowser.Navigate(this.urlTextBox.Text);
+        }
+
+        private void Login()
+        {
+            try
+            {
+                HtmlHelper.SetValueToElement(this.webBrowser.Document, "UserName", ConfigurationManager.AppSettings["SystemLogin.UserName"]);
+                HtmlHelper.SetValueToElement(this.webBrowser.Document, "Password", ConfigurationManager.AppSettings["SystemLogin.Password"]);
+
+                DeathByCaptcha.Client client = (DeathByCaptcha.Client)new DeathByCaptcha.HttpClient(ConfigurationManager.AppSettings["DeathByCaptchaService.UserName"], ConfigurationManager.AppSettings["DeathByCaptchaService.Password"]);
+                DeathByCaptcha.Captcha captcha = client.Decode(
+                    DeathByCaptcha.Client.DefaultTokenTimeout,
+                    new Hashtable() {
+                    { "type", 4 },
+                    { "token_params", "{ \"googlekey\": \"" + this.webBrowser.DocumentText.GetGoogleKey() + "\", \"pageurl\": \"" + URL + "\" }" }
+                    }
+                );
+
+                HtmlHelper.SetValueToElement(this.webBrowser.Document, "g-recaptcha-response", captcha.Text);
+                this.RunActionAndWaitUntilPageLoaded(this.webBrowser, HtmlHelper.ClickElement(this.webBrowser.Document, "action:DoLogin"));
+            }
+            catch (Exception vE)
+            {
+                throw vE;
+            }
+        }
+    }
+}
