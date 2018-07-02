f4914c124b18b26643737cb1943bd2f4a7a6f1af
titiushko <titiushko@gmail.com>
2018-07-02 10:09:57 -0600

    :running::feet:

--- WebsiteClone/FWLA/Sources/workflow-rpa.js
+++ WebsiteClone/FWLA/Sources/workflow-rpa.js
@@ -6,15 +6,14 @@ function updateImage(image) {
     return image.replace(/\.\/images\/icons/gim, "../Sources");
 }
 
-function afterRequest(resource, callback) {
+function afterRequest(resource, type, callback) {
     $(document).ajaxComplete(function (event, jqXHR, ajaxOptions) {
-        if (ajaxOptions.url.indexOf(resource) != -1 && ajaxOptions.type == "POST") {
-            $("#altgrid").find("a").each(function () {
-                $(this).attr("href", updateUrl($(this).attr("href")));
-            });
-            $("#altgrid").find("img").each(function () {
-                $(this).attr("src", updateImage($(this).attr("src")));
-            });
+        if (ajaxOptions.url.indexOf(resource) != -1 && ajaxOptions.type == type) {
+            if (jqXHR.responseText != undefined) {
+                var $element = $("<div/>").append($($.parseHTML(jqXHR.responseText.toString())));
+                $element.find("a").each(function () { $(this).attr("href", updateUrl($(this).attr("href"))); });
+                $element.find("img").each(function () { $(this).attr("src", updateImage($(this).attr("src"))); });
+            }
             if (callback != null && typeof callback == "function") callback();
         }
     });
@@ -60,7 +59,7 @@ var step1 = function () {
     $(".open2.nextbutton").click();
     $("#CLegalID").val("8-310-78");
     $("#getuser").click();
-    afterRequest("check_customer.php", step2);
+    afterRequest("check_customer.php", "GET", step2);
 };
 
 var step2 = function () {
@@ -93,7 +92,7 @@ var step2 = function () {
     $(".open3.nextbutton").click();
     $("#MaVin").val("SJNFBA11Z2201603");
     $("#getcar").click();
-    afterRequest("check_moveable_vin.php", step3);
+    afterRequest("check_moveable_vin.php", "GET", step3);
 };
 
 var step3 = function () {
@@ -113,7 +112,7 @@ var step3 = function () {
     //$("#addCar").val("");
     //$("[name='formBack0']").val("");
     $("#cSubmitA").click();
-    afterRequest("admin_create_contract.php");
+    afterRequest("admin_create_contract.php", "POST", submit);
     //$("#MaEVin").val("");
     //$("#MaEBrand").val("");
     //$("#MaEModel").val("");
@@ -173,6 +172,15 @@ var step3 = function () {
     //$("#cSubmitI").val("");
     //$("#MM_insert").val("");
     //$("#UsrCreated").val("");
+};
+
+var submit = function () {
+    $("#altgrid").find("a").each(function () {
+        $(this).attr("href", updateUrl($(this).attr("href")));
+    });
+    $("#altgrid").find("img").each(function () {
+        $(this).attr("src", updateImage($(this).attr("src")));
+    });
 };
 
 function workflowRpa() {
