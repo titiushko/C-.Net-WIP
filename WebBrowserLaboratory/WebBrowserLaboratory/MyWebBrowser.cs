df247f14c206ff5f6c5f594d1d25ed5db9cf13c2
titiushko <titiushko@gmail.com>
2018-07-04 11:59:27 -0600

    :file_folder: Descargar y subir archivos.

--- WebBrowserLaboratory/WebBrowserLaboratory/MyWebBrowser.cs
+++ WebBrowserLaboratory/WebBrowserLaboratory/MyWebBrowser.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
-using System;
-using WebBrowserLaboratory.Helpers;
 using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using System.Windows.Forms;
+using System;
 using Titiushko.Utilities.Extensions;
+using WebBrowserLaboratory.Helpers;
 
 namespace WebBrowserLaboratory
 {
@@ -13,6 +16,14 @@ namespace WebBrowserLaboratory
         private string SystemWebsite_Password = ConfigurationManager.AppSettings["SystemWebsite.Password"];
         private string DeathByCaptchaService_UserName = ConfigurationManager.AppSettings["DeathByCaptchaService.UserName"];
         private string DeathByCaptchaService_Password = ConfigurationManager.AppSettings["DeathByCaptchaService.Password"];
+
+        private const string WEBBROWSER_TITLE = "Web Browser Laboratory";
+        private const string CLICK_EVENT = "Click";
+        private const string OPEN_BUTTON = "&Open";
+        private const string SAVE_BUTTON = "&Save";
+        private const string CANCEL_BUTTON = "Cancel";
+        private const string ENTER_KEY = "{ENTER}";
+        private const string DEFAULT_ATTACHMENTS_FOLDER = @"C:\ApptividadAttachments\";
 
         public MyWebBrowser()
         {
@@ -25,7 +36,7 @@ namespace WebBrowserLaboratory
         {
             try
             {
-                Fwla();
+                UploadFile();
             }
             catch (Exception vE)
             {
@@ -123,6 +134,143 @@ namespace WebBrowserLaboratory
                     HtmlHelper.ClickElement(this.webBrowser.Document, "className", true, "input", "open1 nextbutton");
                     #endregion
                 }
+            }
+            catch (Exception vE)
+            {
+                throw vE;
+            }
+        }
+
+        private void DownloadFile()
+        {
+            try
+            {
+                string vDownloadDialogTitle = "File Download";
+                string vDownloadDialogSaveButton = SAVE_BUTTON;
+
+                string vSaveAsDialogTitle = "Save As";
+                string vSaveAsDialogSaveButton = SAVE_BUTTON;
+                string vSaveAsDialogFileNameFieldId = "1001";
+                string vFileNameToDownload = "RegistroPrevioInactivo_Detalles_DXCBtn0.pdf";
+
+                string vIdentifierElement = "ctl00_MainContent_BuscadorFolios1_FichaFolio1_ASPxPopupControl1_RegistroPrevioInactivo_DetallesASPxGridView_DXCBtn0";
+
+                // Se crea el task que se encargará de manejar el Dialog de subir adjuntos,
+                // la cual se ejecutará 5 segundos despues de que se le de click a seleccionar archivo
+                Task.Factory.StartNew(() =>
+                {
+                    System.Threading.Thread.Sleep(500);
+                    try
+                    {
+                        AutomationElement vDesktopObject = AutomationElement.RootElement;
+
+                        #region WebBrowserWindow
+                        // Obtener la ventana del robot, por medio del titulo 'RPA'
+                        PropertyCondition vWebBrowserName = new PropertyCondition(AutomationElement.NameProperty, WEBBROWSER_TITLE);
+                        AutomationElement vWebBrowserWindow = vDesktopObject.FindFirst(TreeScope.Children, vWebBrowserName);
+                        #endregion
+
+                        #region DownloadDialog
+                        // Obtener el Dialog de "File Download" por medio del título y el nombre de la ventana que lo levantó
+                        PropertyCondition vDownloadDialogName = new PropertyCondition(AutomationElement.NameProperty, vDownloadDialogTitle);
+                        AutomationElement vDownloadDialogWindow = vWebBrowserWindow.FindFirst(TreeScope.Children, vDownloadDialogName);
+
+                        // Obtener el Button de "Save"
+                        AndCondition vDownloadDialogSaveButtonCondition = new AndCondition(
+                            new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Button),
+                            new PropertyCondition(AutomationElement.NameProperty, vDownloadDialogSaveButton)
+                        );
+                        AutomationElement vDownloadDialogSaveButtonElement = vWebBrowserWindow.FindFirst(TreeScope.Descendants, vDownloadDialogSaveButtonCondition);
+
+                        // Establecer el foco al Button de "Save" y hacer {{Enter}}
+                        if (vDownloadDialogSaveButtonElement != null)
+                        {
+                            vDownloadDialogSaveButtonElement.SetFocus();
+                            SendKeys.SendWait(ENTER_KEY);
+                        }
+                        #endregion
+
+                        #region SaveAsDialog
+                        // Obtener el Dialog de "File SaveAs" por medio del título y el nombre de la ventana que lo levantó
+                        PropertyCondition vSaveAsDialogName = new PropertyCondition(AutomationElement.NameProperty, vSaveAsDialogTitle);
+                        AutomationElement vSaveAsDialogWindow = vWebBrowserWindow.FindFirst(TreeScope.Children, vSaveAsDialogName);
+
+                        // Obtener el Field de "File name" utilizando ControlId, para esto se usa el Spy++ (Visual Studio 2013: Menú > Tools > Spy++)
+                        AndCondition vSaveAsDialogFileNameFieldCondition = new AndCondition(
+                            new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit),
+                            new PropertyCondition(AutomationElement.AutomationIdProperty, vSaveAsDialogFileNameFieldId)
+                        );
+                        AutomationElement vSaveAsDialogFileNameFieldElement = vSaveAsDialogWindow.FindFirst(TreeScope.Descendants, vSaveAsDialogFileNameFieldCondition);
+                        ValuePattern vSaveAsDialogFileNameFieldValuePattern = vSaveAsDialogFileNameFieldElement.GetCurrentPattern(ValuePattern.Pattern) as ValuePattern;
+
+                        // Establecer la dirección y nombre del archivos con extensión que se va a descargar
+                        vSaveAsDialogFileNameFieldValuePattern.SetValue(DEFAULT_ATTACHMENTS_FOLDER + vFileNameToDownload);
+
+                        // Obtener el Button de "Save"
+                        AndCondition vSaveAsDialogSaveButtonCondition = new AndCondition(
+                            new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Button),
+                            new PropertyCondition(AutomationElement.NameProperty, vSaveAsDialogSaveButton)
+                        );
+                        AutomationElement vSaveAsDialogSaveButtonElement = vWebBrowserWindow.FindFirst(TreeScope.Descendants, vSaveAsDialogSaveButtonCondition);
+
+                        // Establecer el foco al Button de "Save" y hacer {{Enter}}
+                        if (vSaveAsDialogSaveButtonElement != null)
+                        {
+                            vSaveAsDialogSaveButtonElement.SetFocus();
+                            SendKeys.SendWait(ENTER_KEY);
+                        }
+                        #endregion
+                    }
+                    catch (Exception vE)
+                    {
+                        throw vE;
+                    }
+                });
+
+                // Hacer click al enlace para descargar el archivo
+                HtmlHelper.ClickElement(this.webBrowser.Document, vIdentifierElement, true);
+            }
+            catch (Exception vE)
+            {
+                throw vE;
+            }
+        }
+
+        private void UploadFile()
+        {
+            try
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    System.Threading.Thread.Sleep(500);
+                    try
+                    {
+                        AutomationElement vDesktopObject = AutomationElement.RootElement;
+                        PropertyCondition vWebBrowserName = new PropertyCondition(AutomationElement.NameProperty, WEBBROWSER_TITLE);
+                        AutomationElement vWebBrowserWindow = vDesktopObject.FindFirst(TreeScope.Children, vWebBrowserName);
+                        PropertyCondition vUploadDialogName = new PropertyCondition(AutomationElement.NameProperty, "Choose File to Upload");
+                        AutomationElement vUploadDialogWindow = vWebBrowserWindow.FindFirst(TreeScope.Children, vUploadDialogName);
+                        AndCondition vUploadDialogFileNameFieldCondition = new AndCondition(
+                            new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit),
+                            new PropertyCondition(AutomationElement.AutomationIdProperty, "1148")
+                        );
+                        AutomationElement vUploadDialogFileNameFieldElement = vUploadDialogWindow.FindFirst(TreeScope.Descendants, vUploadDialogFileNameFieldCondition);
+                        ValuePattern vUploadDialogFileNameFieldValuePattern = vUploadDialogFileNameFieldElement.GetCurrentPattern(ValuePattern.Pattern) as ValuePattern;
+                        vUploadDialogFileNameFieldValuePattern.SetValue(DEFAULT_ATTACHMENTS_FOLDER + "report_108370888.pdf");
+                        AndCondition vUploadDialogOpenButtonCondition = new AndCondition(
+                            new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Button),
+                            new PropertyCondition(AutomationElement.NameProperty, CANCEL_BUTTON)
+                        );
+                        AutomationElement vUploadDialogOpenButtonElement = vWebBrowserWindow.FindFirst(TreeScope.Descendants, vUploadDialogOpenButtonCondition);
+                        vUploadDialogOpenButtonElement.SetFocus();
+                        SendKeys.SendWait(ENTER_KEY);
+                    }
+                    catch (Exception vE)
+                    {
+                        throw vE;
+                    }
+                });
+                HtmlHelper.ClickElement(this.webBrowser.Document, "UCCargarDocAutorizacion1_flDocumento", true);
             }
             catch (Exception vE)
             {
