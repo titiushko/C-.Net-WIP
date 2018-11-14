6268b9b61d4089ba26616e7d54ca34deb570c7b5
Titiushko <titiushko@gmail.com>
2018-11-14 10:51:29 -0600

    :mouse: Extensión para Controller Se crea el método para obtener el HtmlHelper desde un Controller

---
+++ Titiushko/Titiushko.MVC5/Extensions/ControllerExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace Titiushko.MVC5.Extensions
+{
+    public static class ControllerExtension
+    {
+        public static HtmlHelper GetHtmlHelper(this Controller pController)
+        {
+            ViewContext vViewContext = new ViewContext(pController.ControllerContext, new FakeView(), pController.ViewData, pController.TempData, TextWriter.Null);
+            return new HtmlHelper(vViewContext, new ViewPage());
+        }
+    }
+
+    public class FakeView : IView
+    {
+        public void Render(ViewContext pViewContext, TextWriter pTextWriter)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
