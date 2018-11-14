25fa78e590eafc7deec0aa4d92f3db9611390e5d
Titiushko <titiushko@gmail.com>
2018-11-14 17:11:03 -0600

    :egg: Creación de anotación personalizada para obtener un DisplayName desde los Resources

---
+++ Titiushko/Titiushko.MVC5/Models/Customizations/LocalizedDisplayNameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Titiushko.MVC5.Models.Customizations
+{
+    /// <summary>
+    /// Obtener el DisplayName de los Resources
+    /// </summary>
+    public class LocalizedDisplayNameAttribute : DisplayNameAttribute
+    {
+        public LocalizedDisplayNameAttribute(string pResource)
+            : base(GetMessageFromResource(pResource))
+        { }
+
+        private static string GetMessageFromResource(string pResource)
+        {
+            return Resources.Functions.GetResource(pResource);
+        }
+    }
+}
