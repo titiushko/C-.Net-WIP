f6f0e89011f2b43fa3c6e1f0195719a897aa3e52
Titiushko <titiushko@gmail.com>
2018-11-21 07:31:40 -0600

    :turtle: Método extensión para convertir un Int a Enum

---
+++ Titiushko/Titiushko.Utilities/Extensions/IntExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Titiushko.Utilities.Extensions
+{
+    public static class IntExtension
+    {
+        /// <summary>
+        /// Convierte un Int a Enum de tipo T
+        /// </summary>
+        /// <typeparam name="T">Tipo de Enum de la conversión</typeparam>
+        /// <param name="vValue">Int a convertir</param>
+        /// <param name="vDefaultValue">Valor predeterminado si "vValue" es null o no se pueda realizar la conversión</param>
+        /// <returns>Enum de tipo T</returns>
+        public static T ToEnum<T>(this int vValue, T vDefaultValue)
+        {
+            try
+            {
+                if (vValue == 0) return vDefaultValue;
+                return (T)Enum.ToObject(typeof(T), vValue);
+            }
+            catch
+            {
+                return vDefaultValue;
+            }
+        }
+    }
+}
